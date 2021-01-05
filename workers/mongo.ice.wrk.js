4181f81f68b572ceab18eebe556ebc748a93b5c7
Paolo Ardoino <paolo@bitfinex.com>
2021-01-05 17:29:13 +0000

    mc

--- workers/mongo.ice.wrk.js
+++ workers/mongo.ice.wrk.js
@@ -86,7 +86,7 @@ class WrkWrkIceMongo extends WrkBase {
     async.series([
       next => { super._start(next) },
       next => {
-        this.interval_0.add('export', this.export.bind(this), 2500)
+        this.interval_0.add('export', this.export.bind(this), 1500)
         next()
       }
     ], cb)
